45a685fe4cfb4e077c3562db0094d449b862dcf1
prateekrathore@rivia.com <prateekrathore@rivian.com>
2025-06-10 14:52:54 +0530

    Initial Commit

---
+++ Puff Puff Pass/Puff_Puff_PassApp.swift
@@ -0,0 +1,17 @@
+//
+//  Puff_Puff_PassApp.swift
+//  Puff Puff Pass
+//
+//  Created by Prateek Rathore on 10/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Puff_Puff_PassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
